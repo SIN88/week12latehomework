852eb4aabf2385a49c20d12e9d9743e9d324a5ef
SIN88 <KG19882020@GMAIL.COM>
2021-08-17 11:50:26 -0700

    asking slack

--- db/connection.js
+++ db/connection.js
@@ -3,15 +3,23 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
- 
+  port:3306,
   user: "root",
-  
   password: "Persephone1",
   database: "employees"
 });
 
-connection.connect();
-
-connection.query = inquirer.promisify(connection.query);
-
-module.exports = connection;
+const afterConnection = () => {
+    connection.query('SELECT * FROM employees', (err, res) => {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  };
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    afterConnection();
+  });
+  
